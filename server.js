6bbbdba8e6f2423c7cd886ebb9bcfefd867d17a2
István Ignácz <ignaczistvan@gmail.com>
2016-10-24 13:27:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Zephyr')
+  response.send('Zephyr test')
 })
 
 app.listen(app.get('port'), function() {
