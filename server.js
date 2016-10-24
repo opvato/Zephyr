e002c4beff5f87bb7f24e5934ea8363f263dd839
Robert Horvath <opvato@gmail.com>
2016-10-24 13:26:26 +0200

    log env.mode

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ firebase.initializeApp({
   databaseURL: "https://op-flight-b85b9.firebaseio.com/"
 });
 
+console.log(process.env.mode);
 
 
 var db = firebase.database();
@@ -162,5 +163,5 @@ app.get('/', function(request, response) {
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at localhost:" + app.get('port'))    
 })
